13a16acf311daa129c187a864ac83f1a83ccdc35
darkunk <118312765+darkunk@users.noreply.github.com>
2025-01-20 10:56:35 +0100

    Initial commit from your app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,10 +24,10 @@ import React from 'react'
                 <div className="relative flex-1">
                   <input
                     type="text"
-                    placeholder="Search items..."
-                    className="pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary w-full"
+                    placeholder="Search for anything..."
+                    className="pl-4 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary w-full"
                   />
-                  <IonIcon icon={searchOutline} className="absolute left-3 top-2.5 text-gray-400 w-5 h-5" />
+                  <IonIcon icon={searchOutline} className="absolute right-3 top-2.5 text-gray-400 w-5 h-5" />
                 </div>
 
                 {/* Right Section */}
